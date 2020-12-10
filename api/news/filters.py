9b1dcfc4e4afa4ba825b1a52d1a0f008c7426cd2
timkatala <55878204+timkatala@users.noreply.github.com>
2020-12-10 18:33:55 +0500

    Update filters.py

--- api/news/filters.py
+++ api/news/filters.py
@@ -2,7 +2,7 @@ import django_filters
 from rest_framework import filters
 
 class NewsFilter(filters.FilterSet):
-    timestamp_gte = django_filters.DateTimeFilter(name="timestamp", lookup_expr='gte')
+    timestamp_gte = django_filters.DateTimeFilter(name="date_joined", lookup_expr='gte')
     class Meta:
         model = Event
-        fields = ['tags', 'date_joined', 'timestamp_gte']
+        fields = ['tags', 'date_joined', 'date_joined_gte']
